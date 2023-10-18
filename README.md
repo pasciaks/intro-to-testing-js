d7750f0ff483d8def6a59126458979525140cb41
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-18 07:59:29 -0600

    Lesson progress.

--- README.md
+++ README.md
@@ -170,7 +170,7 @@ Our next exercise is to follow the TDD workflow to develop incremental tests and
 - Step 4: Run all tests. Does each test turn green? If so, then we can proceed. 
 - Step 5: Refactor! It's definitely refactoring time!
 
-### When to Refactor
+### When to Refactor ((( LEFT OFF HERE )))
 - How do we know that it's time to refactor? The answer: Once we have a handful of green tests, but the logic feels hard-coded, funky, or incomplete, then it's probably refactoring time.
 - Notice that when the input is "Jane", "Pat", or "Alex", the tests green. But what if we sent in any other name as the argument? 
 - When every new test means that we're adding another `if` or `else if` to the code, is there a better way of doing things?
--- tests.js
+++ tests.js
@@ -29,17 +29,17 @@ describe('sayHello', function() {
     });
 });
 
-// describe('sayHello', function() {
-//     it('should be a defined function', function() {
-//         expect(typeof sayHello).toBe('function');
-//     });
-//     it('should return a string when called', function() {
-//         expect(typeof sayHello()).toBe("string");
-//     });
-//     it('should return the string "Hello, Sheldon!" when executed', function() {
-//         expect(sayHello('Sheldon')).toBe("Hello, Sheldon!");
-//     });
-//     it("should never return 'undefined' when called", function() {
-//         expect(sayHello()).not.toBe(undefined);
-//     });
-// });
+describe('sayHello', function () {
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Sheldon!" when executed', function() {
+        expect(sayHello('Sheldon')).toBe("Hello, Sheldon!");
+    });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello('Alex')).toBe("Hello, Alex!");
+    });
+    it('should return the string "Hello, Pat!" when executed', function() {
+        expect(sayHello('Pat')).toBe("Hello, Pat!");
+    });
+});
