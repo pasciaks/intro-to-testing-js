0bb5ee7bb988ec3fb108620e6c4a586e33a5bd64
Sheldon Pasciak <sheldon@pasciak.com>
2023-10-18 10:14:49 -0600

    Testing unit complete, with NaN issue fixed.

--- code.js
+++ code.js
@@ -42,3 +42,52 @@ console.log(isEven(2));
 console.log(isEven(2));
 console.log(isEven(2));
 
+function isVowel(letter) {
+    if (typeof letter === 'undefined') {
+        return false;
+    }
+    if (letter === null) {
+        return false;
+    }
+    if (letter === '') {
+        return false;
+    }
+    if (letter.length>1) {
+        return false;
+    }
+    if (typeof letter === 'boolean') {
+        return false;
+    }
+    return ['a', 'e', 'i', 'o', 'u'].includes(`${letter}`.toLowerCase());
+}
+
+// @todo - add in test code to tests.js
+
+console.log(isVowel);
+console.log(isVowel("a"));
+console.log(isVowel("A"));
+console.log(isVowel("y"));
+console.log(isVowel(4));
+console.log(isVowel(true));
+console.log(isVowel(false));
+console.log(isVowel("banana"));
+console.log(isVowel());
+
+// @todo - add in test code to tests.js
+
+function add(num1,num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return Number(num1 + num2);
+}
+
+console.log(add(2,3));
+console.log(add(-3,-9));
+console.log(add("5",6));
+console.log(add("-4","10"));
+console.log(add("banana","split"));
+console.log(add(2,"apples"));
+console.log(add());
+
+
+
